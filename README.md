fa1c4350d2b37ea38aa21d89b1e70a0038ca302a
isagajdak <isagajdak@jetlend.ru>
2022-12-09 03:07:38 +0200

    ref: redesign

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PaymentSite
 Frontend for https://github.com/blueflyingpanda/Payment
+
+Remote payment system for economic game
--- main.js
+++ main.js
@@ -18,10 +18,11 @@ class RequestsSender {
         var xmlHttp = new XMLHttpRequest();
         let callback = this.callback
         xmlHttp.onreadystatechange = function () {
-            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            if (xmlHttp.readyState == 4)
                 callback(xmlHttp.responseText);
         }
         xmlHttp.open("GET", this.url + "/" + path, this.isAsync);
+        xmlHttp.setRequestHeader("Authorization", localStorage.getItem("Authorization"))
         xmlHttp.send(null);
     }
 
@@ -29,49 +30,101 @@ class RequestsSender {
         var xmlHttp = new XMLHttpRequest();
         let callback = this.callback
         xmlHttp.onreadystatechange = function () {
-            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            if (xmlHttp.readyState == 4)
                 callback(xmlHttp.responseText);
         }
         xmlHttp.open("POST", this.url + "/" + path, this.isAsync);
+        xmlHttp.setRequestHeader("Authorization", localStorage.getItem("Authorization"))
         xmlHttp.send(data);
     }
 }
 
-const url = "https://lhelper.pythonanywhere.com";  // "http://127.0.0.1:5000"
 
-let rs = new RequestsSender(url, RequestsSender.logCallback);
+const baseURL = "http://127.0.0.1:5500"; // "https://blueflyingpanda.github.io/PaymentSite/"
+const apiURL = "http://127.0.0.1:5000";  // "https://lhelper.pythonanywhere.com";
+const tokenHeader = "auth_token";
+
+let rs = new RequestsSender(apiURL, htmlAuthCallback);
+
+function logout() {
+    localStorage.removeItem("Authorization")
+    window.location.replace(`${baseURL}/index.html`);
+}
+
+function getTeacherPage() {
+    rs.callback = htmlTeacherCallback;
+    rs.httpGet('teacher');
+
+}
+
+function htmlTeacherCallback(text) {
+    let data = JSON.parse(text);
+    if (data["status"] == 200) {
+        firstName = data["teacher"][0][0];
+        middleName = data["teacher"][0][1];
+        money = data["teacher"][0][3];
+        greeting = document.getElementById("greeting");
+        greeting.innerHTML += ` ${firstName} ${middleName}!`;
+        balance = document.getElementById("balance");
+        balance.innerHTML += ` ${money} ШкололоКоинов`;
+    }
+    else {
+        alert(`${data["status"]} ${data["message"][0]}`)
+        window.location.replace(`${baseURL}/index.html`);
+    }
+}
+
+function getPlayerPage() {
+    rs.callback = htmlPlayerCallback;
+    rs.httpGet('player');
+
+}
+
+function htmlPlayerCallback(text) {
+    let data = JSON.parse(text);
+    if (data["status"] == 200) {
+        firstName = data["player"][0][0];
+        lastName = data["player"][0][2];
+        gradeInfo = data["player"][0][3];
+        money = data["player"][0][4];
+        console.log(data)
+        greeting = document.getElementById("greeting");
+        greeting.innerHTML += ` ${firstName} ${lastName}!`;
+        grade = document.getElementById("grade");
+        grade.innerHTML += ` ${gradeInfo}`
+        balance = document.getElementById("balance");
+        balance.innerHTML += ` ${money} ШкололоКоинов`;
+    }
+    else {
+        alert(`${data["status"]} ${data["message"][0]}`)
+        window.location.replace(`${baseURL}/index.html`);
+    }
+}
+
+function htmlAuthCallback(text) {
+    let data = JSON.parse(text);
+    if (data["status"] == 200) {
+        localStorage.setItem("Authorization", data[tokenHeader])
+        if (data["teacher"] == true) {
+            window.location.replace(`${baseURL}/teacher.html`);
+        }
+        else {
+            window.location.replace(`${baseURL}/player.html`);
+        }
+    }
+    else {
+        alert(data["status"]);
+    }
+}
 
 function main() {
     addEventListener("submit", (e) => {
         e.preventDefault();
-        rs.callback = RequestsSender.logCallback
-        let form = document.getElementById('studentForm');
-        rs.httpPost('student', new FormData(form));
+        rs.callback = htmlAuthCallback
+        let form = document.getElementById('authForm');
+        let formData = new FormData(form);
+        formData.set("password", sha256(formData.get("password")))
+        rs.httpPost('auth', formData);
         form.reset();
     })
-}
-
-function htmlCustomCallback(text) {
-    let data = JSON.parse(text);
-    if (data["status"] == 200) {
-        let output = document.getElementById("output")
-        for (let i = 0; i < data["message"].length; i++) {
-            output.innerHTML += `<h1>${data["message"][i][3]} ${data["message"][i][1]} ${data["message"][i][2]}</h1>`;
-            output.innerHTML += `<h2>Класс: ${data["message"][i][4]}</h2>`;
-            output.innerHTML += `<h3>Почта: ${data["message"][i][5]}</h3>`;
-            output.innerHTML += `<h4>ШкололоКоинов: ${data["message"][i][6]}</h4>`;
-            output.innerHTML += `<hr>`;
-        }
-    }
-
-}
-
-function showStudents() {
-    clearStudents()
-    rs.callback = htmlCustomCallback
-    rs.httpGet()
-}
-
-function clearStudents() {
-    document.getElementById("output").innerHTML = '<button onclick="showStudents()">показать список</button><button onclick="clearStudents()">очистить список</button>';
 }
