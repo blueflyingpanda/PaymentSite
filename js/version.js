6d57628d6384c7f2a58dc1311c33dd39ee926b3a
FloppedWaffle <Hellers12372@gmail.com>
2023-02-11 11:06:56 +0300

    Changes to be committed: 	new file:   js/version.js

---
+++ js/version.js
@@ -0,0 +1,7 @@
+let version = 1;
+
+document.write(`<script src="js/main.js?ver=${version}"></script>`);
+document.write(`<script src="js/script.js?ver=${version}"></script>`);
+document.write(`<script src="js/sha256.js?ver=${version}}"></script>`);
+
+document.write(`<link rel="stylesheet" href="css/style.css?ver=${version}">`);
