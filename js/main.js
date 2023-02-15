ed909ff23b5268d8a6bcb7f5fe186d91eb35073b
FloppedWaffle <Hellers12372@gmail.com>
2023-02-15 20:12:43 +0300

    upd: firms, diagram

--- js/main.js
+++ js/main.js
@@ -54,8 +54,8 @@ if (frontProduction) {
     // baseURL = "https://game.school1598.ru";
 }
 if (backProduction) {
-    // apiURL = "https://bgame.school1598.ru:5000";
-    apiURL = "https://lhelper.pythonanywhere.com";
+    apiURL = "https://bgame.school1598.ru:5000";
+    // apiURL = "https://lhelper.pythonanywhere.com";
 }
 
 const tokenHeader = "auth_token";
